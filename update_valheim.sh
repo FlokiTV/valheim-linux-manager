a3bf5b1c006dcff419ed945c7d605ed94a5ae150
Dawyson Wüster de Souza Frossard <dawysonw@gmail.com>
2021-03-05 10:15:22 -0300

    Update update_valheim.sh

--- update_valheim.sh
+++ update_valheim.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./Steam/steamcmd.sh +login anonymous +force_install_dir "$PWD/vhserver" +app_update 896660 validate +exit
+./Steam/steamcmd.sh +login anonymous +force_install_dir "/root/vhserver" +app_update 896660 validate +exit
