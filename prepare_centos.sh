39921d1b7f51ff8d95f6d79a78e2fcad62427780
Dawyson Wüster de Souza Frossard <dawysonw@gmail.com>
2021-03-05 10:07:38 -0300

    Update prepare_centos.sh

--- prepare_centos.sh
+++ prepare_centos.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-yum -y install epel-release screen curl wget tar bzip2 gzip unzip python3 binutils bc jq tmux glibc.i686 libstdc++ libstdc++.i686 htop
+yum -y install epel-release
+yum -y install screen curl wget tar bzip2 gzip unzip python3 binutils bc jq tmux glibc.i686 libstdc++ libstdc++.i686 htop
