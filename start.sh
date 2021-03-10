eebba724a126b91ecfe9946910831949aca13d03
Dawyson Wüster de Souza Frossard <dawysonw@gmail.com>
2021-03-10 17:52:50 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 screen -S "vhserver" -d -m -L
-screen -r "vhserver" -X stuff 'cd vhserver && nice -20 ./start_server.sh \n'
+screen -r "vhserver" -X stuff 'cd ../vhserver && nice -20 ./start_server.sh \n'
