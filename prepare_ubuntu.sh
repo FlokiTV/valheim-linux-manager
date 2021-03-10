63f0d6356b78f629a1fb630d7bd7229037bac7af
Dawyson Wüster de Souza Frossard <dawysonw@gmail.com>
2021-03-10 18:01:22 -0300

    Update prepare_ubuntu.sh

--- prepare_ubuntu.sh
+++ prepare_ubuntu.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo dpkg --add-architecture i386; sudo apt update; sudo apt install curl wget file tar bzip2 gzip unzip bsdmainutils python util-linux ca-certificates binutils bc jq tmux netcat lib32gcc1 lib32stdc++6
+sudo dpkg --add-architecture i386; sudo apt update -y; sudo apt install -y curl wget file tar bzip2 gzip unzip bsdmainutils python util-linux ca-certificates binutils bc jq tmux netcat lib32gcc1 lib32stdc++6
