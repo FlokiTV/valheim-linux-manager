0976dd1def8c6e5dcd70572d2dea5407a7940a12
Dawyson Wüster de Souza Frossard <dawysonw@gmail.com>
2021-03-10 18:33:16 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 screen -S "vhserver" -d -m -L
-screen -r "vhserver" -X stuff 'cd ../vhserver && nice -20 ./start_server.sh \n'
+screen -r "vhserver" -X stuff './webhook.sh "Server Start 🟢" && cd ../vhserver && nice -20 ./start_server.sh && cd ../vh && ./webhook.sh "Server Stop ⛔" \n'
