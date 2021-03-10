9e8eb9a160df082111411ac6d8c7d3998ceb860b
Dawyson Wüster de Souza Frossard <dawysonw@gmail.com>
2021-03-10 18:32:01 -0300

    Update webhook.sh

--- webhook.sh
+++ webhook.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
 
+export WEBHOOK_URL="https://discord.com/api/webhooks/813398238514118686/ETVd55tGVUXJVRqkIvnT_GetE0tSPn9RU-5bXLxc0T8PWDyf48hAXY7Wdm-Qcnz9ZrG2"
+curl -X POST \
+  -H "Content-Type: application/json" \
+  -d '{"username": "SkyReds", "content": "'"$@"'"}' \
+  $WEBHOOK_URL
